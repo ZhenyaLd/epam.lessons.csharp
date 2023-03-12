a81856de7ebc20ae69b426ac28ac6fdcc4c8f7c8
ZhenyaLd <127652154+ZhenyaLd@users.noreply.github.com>
2023-03-12 10:01:42 +0000

    first lesson

---
+++ lesson_01/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool isTrue = true;
+bool isFalse = false;
+
+//integer values from -2,147,483,648 to 2,147,483,647
+int age = 20;
+
+//
+long value = 10000000000000000;
+
+float pi = 3.14f;
+double pi2 = 3.14;
+decimal pi3 = 3.14m;
+
+string name = "Alex";
+
+
+// arithmetic operations +, -, *, /, %
+
+
+
+Console.WriteLine("Hello, World!");
