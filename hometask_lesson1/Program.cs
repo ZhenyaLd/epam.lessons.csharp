47c07d60b2314eddbd72e292e340f431a81bc75d
ZhenyaLd <127652154+ZhenyaLd@users.noreply.github.com>
2023-03-19 07:57:09 +0000

    hometask1

---
+++ hometask_lesson1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+double calc_num;
+double degree;
+
+degree = Math.Pow(6,2);
+
+calc_num = (degree-1)/2;
+
+Console.WriteLine(calc_num);
+ 
